da5551f510723c186c6470145da601e9bb81b89a
Magdalena Checinski <madzix.p730@gmail.com>
2020-07-05 15:02:30 +0200

    added functions that change the body background and  color to a separate block

--- script.js
+++ script.js
@@ -2,25 +2,27 @@
   const hallo = () => {
     console.log("Witaj developerze(-rko), miłego code review ;)")
   }
+} {
+  let body = document.querySelector("body");
+  let fontColor = document.querySelector(".js-fontColor");
+  let backgroundColor = document.querySelector(".js-backgroundColor");
+
+  fontColor.addEventListener('click', () => {
+    body.style.color = "indigo";
+  });
+  backgroundColor.addEventListener('click', () => {
+    body.style.backgroundColor = "#bbb";
+  });
 }
-let fontColor = document.querySelector(".js-fontColor");
-let backgroundColor = document.querySelector(".js-backgroundColor");
 let border = document.querySelector(".js-border");
 let changeFontSize = document.querySelector(".js-fontSize");
 let main = document.querySelector("main");
-let body = document.querySelector("body");
 let mode = document.querySelector('.js-mode');
 
 let initialFontSize = 20;
 
 
 
-fontColor.addEventListener('click', () => {
-  body.style.color = "indigo";
-});
-backgroundColor.addEventListener('click', () => {
-  body.style.backgroundColor = "#bbb";
-});
 border.addEventListener('click', () => {
   main.style.border = "5px magenta dashed ";
 });
